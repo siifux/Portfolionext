a61b572c437a2b7bd9a84f61b2474f9b7d82b9e5
Sindre <sindre.furulund@puzzlepart.com>
2022-10-09 17:38:49 +0200

    Styling About Component

--- components/About.tsx
+++ components/About.tsx
@@ -5,13 +5,18 @@ function About() {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      transition={{ duration: 1.5 }}
       whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.2 }}
       className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <motion.h3
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.2 }}
+        className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl"
+      >
         About
-      </h3>
+      </motion.h3>
 
       <motion.img
         initial={{ x: -200, opacity: 0 }}
@@ -38,7 +43,7 @@ function About() {
           a big photo enthusiast, often roaming the streets taking photos of the
           world and the people I meet along the way. <br />
           <br />
-          Skills / Interests: JavaScript, React, HTML5, CSS, Git, Github,
+          <span className="uppercase text-gray-400 tracking-[3px] font-bold">Skills & Interests:</span>{" "}JavaScript, React, HTML5, CSS, Git, Github,
           Angular, Vue, Testing, ASP.NET, SQL.
         </p>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Portfolio</title>
+        <title>Sindre Furulund</title>
       </Head>
 
       <Header />
